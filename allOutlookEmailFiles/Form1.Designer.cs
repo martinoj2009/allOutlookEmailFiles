b304c3feeb34bc4c5bb81eace34ca28314e7379d
Martino <martinojones_2009@hotmail.com>
2016-03-22 17:02:09 -0400

    Works with office 16.0

--- allOutlookEmailFiles/Form1.Designer.cs
+++ allOutlookEmailFiles/Form1.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.registryUsers = new System.Windows.Forms.ListBox();
+            this.allOutlookFiles = new System.Windows.Forms.ListBox();
+            this.GETFILES = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // registryUsers
+            // 
+            this.registryUsers.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.registryUsers.FormattingEnabled = true;
+            this.registryUsers.Location = new System.Drawing.Point(12, 50);
+            this.registryUsers.Name = "registryUsers";
+            this.registryUsers.Size = new System.Drawing.Size(222, 316);
+            this.registryUsers.TabIndex = 0;
+            this.registryUsers.SelectedIndexChanged += new System.EventHandler(this.registryUsers_SelectedIndexChanged);
+            // 
+            // allOutlookFiles
+            // 
+            this.allOutlookFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.allOutlookFiles.FormattingEnabled = true;
+            this.allOutlookFiles.HorizontalScrollbar = true;
+            this.allOutlookFiles.Location = new System.Drawing.Point(240, 50);
+            this.allOutlookFiles.Name = "allOutlookFiles";
+            this.allOutlookFiles.Size = new System.Drawing.Size(299, 316);
+            this.allOutlookFiles.TabIndex = 1;
+            this.allOutlookFiles.SelectedIndexChanged += new System.EventHandler(this.allOutlookFiles_SelectedIndexChanged);
+            // 
+            // GETFILES
+            // 
+            this.GETFILES.DoWork += new System.ComponentModel.DoWorkEventHandler(this.GETFILES_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 378);
+            this.Controls.Add(this.allOutlookFiles);
+            this.Controls.Add(this.registryUsers);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox registryUsers;
+        private System.Windows.Forms.ListBox allOutlookFiles;
+        private System.ComponentModel.BackgroundWorker GETFILES;
     }
 }
 
--- allOutlookEmailFiles/Form1.cs
+++ allOutlookEmailFiles/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,77 @@ namespace allOutlookEmailFiles
         public Form1()
         {
             InitializeComponent();
+
+            main();
+        }
+
+
+        private void main()
+        {
+            //Get all the SIDS in the registry
+            string[] users = Registry.Users.GetSubKeyNames();
+
+            foreach(string user in users)
+            {
+                //As long as the string isn't the DEFAULT user then continue
+                if(!(user == ".DEFAULT"))
+                {
+                    registryUsers.Items.Add(user);
+                }
+                
+            }
+
+            
+
+        }
+
+
+        //This is the background worker that will get the files
+        private void GETFILES_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        //Perform this task when a SID is selected
+        private void registryUsers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RegistryKey usersRegistry = Registry.Users.OpenSubKey(registryUsers.SelectedItem.ToString() + @"\SOFTWARE\Microsoft\Office\16.0\Outlook\Search");
+
+            string[] outlookFiles = null;
+            try
+            {
+                outlookFiles = usersRegistry.GetValueNames();
+            }
+            catch(Exception)
+            {
+
+            }
+            
+            if(outlookFiles != null)
+            {
+                allOutlookFiles.Items.Clear();
+                foreach (string name in outlookFiles)
+                {
+                    allOutlookFiles.Items.Add(name);
+                }
+            }
+            else
+            {
+                allOutlookFiles.Items.Clear();
+                allOutlookFiles.Items.Add("NO FILES FOUND!");
+            }
+            
+        }
+
+        private void allOutlookFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string[] selectedItems;
+
+            foreach(string item in allOutlookFiles.SelectedItems)
+            {
+
+            }
+
         }
     }
 }
